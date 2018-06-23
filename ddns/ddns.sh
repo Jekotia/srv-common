36c770372f9855b30488b55c695cca00ad65dcbd
Josh 'Jekotia' Ameli <contact@jekotia.net>
2018-06-23 19:11:10 +0000

    DDNS moved to individual server config

--- ddns/ddns.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-source ${_SCRIPT_INIT}
-source ${_SCRIPT_ROOT}/ddns.conf
-
-duckURL="https://www.duckdns.org/update?domains=${duckDomains}&token=${duckToken}&ip=${duckIP}"
-
-echo url="${duckURL}" | curl -k -o ${_LOGS}/ddns.log -K -
